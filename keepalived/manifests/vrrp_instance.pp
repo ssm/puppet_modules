125e4323163921e5d04527a471f67f309ad5fe1e
Stig Sandbeck Mathisen <ssm@linpro.no>
2008-10-13 14:02:25 +0200

    Update interface names in comments

--- keepalived/manifests/vrrp_instance.pp
+++ keepalived/manifests/vrrp_instance.pp
@@ -7,17 +7,16 @@
 # include keepalived
 # keepalived::vrrp_instance { 
 # 
-#   "eth0":
+#   "vlan101":
 #     virtual_ipaddress => "192.0.2.1/32";
 # 
-#   "eth0-foo":
+#   "vlan101-foo":
 #     ensure            => absent,
-#     interface         => "eth0",
+#     interface         => "vlan101",
 #     virtual_ipaddress => ["192.0.2.2/32",
 #                           "192.0.2.3/32"];
 # 
-#   "eth0-bar":
-#     interface         => "eth0",
+#   "vlan102":
 #     virtual_ipaddress => ["192.0.2.4/32"],
 #     virtual_routes    => ["192.0.2.128/30",
 #                           "192.0.2.132/30 via 102.0.2.130"
